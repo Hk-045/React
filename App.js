68a888494cfd55d86a60f5123c76f7cfc64f51a6
Harish <harishsharma4789048@gmail.com>
2026-06-15 14:09:30 +0530

    Added React and Parcel setup

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1",{id:"heading"},"Hello World from React")
 const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"Heading"),React.createElement("h2",{id:"heading"},"Heading2")]),React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"Heading"),React.createElement("h2",{id:"heading"},"Heading2")])])
 const root = ReactDOM.createRoot(document.getElementById("root"));
